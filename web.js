45359ae90c2ab9abc47f73aa7c8022ac99f6aeef
Pelment <blond.programmist@gmail.com>
2013-07-06 12:28:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('That's new string of mine');
 });
 
 var port = process.env.PORT || 5000;
